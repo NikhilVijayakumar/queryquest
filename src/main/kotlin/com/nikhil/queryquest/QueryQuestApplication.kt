ee4adc7e1874c426f52e50d4d4239dbdb8d06edd
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2023-07-04 22:46:17 +0530

    Initial commit

---
+++ src/main/kotlin/com/nikhil/queryquest/QueryQuestApplication.kt
@@ -0,0 +1,11 @@
+package com.nikhil.queryquest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QueryQuestApplication
+
+fun main(args: Array<String>) {
+	runApplication<QueryQuestApplication>(*args)
+}
---
+++ src/test/kotlin/com/nikhil/queryquest/QueryquestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nikhil.queryquest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QueryquestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
